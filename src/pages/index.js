9463f7acc8e09254b53905b8889de5932cf619ac
Randall Pace <randall@rpx.io>
2021-01-09 17:35:33 -0500

    add posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,29 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div class="pt-8">
       <Head>
-        <title>rpx.io</title>
+        <title>Randall Pace</title>
       </Head>
-      <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        helloworld
-      </h1>
+      <h2 className="text-3xl leading-9 font-semibold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+        Posts
+      </h2>
+      <ul className="pt-4 pb-6 list-disc space-y-1 mx-4">
+        <li>
+          <a href="#" className="text-indigo-600 font-semibold">Posting about that one thing</a>
+        </li>
+        <li>
+          <a href="#" className="text-indigo-600 font-semibold">One more thing...</a>
+        </li>
+        <li>
+          <a href="#" className="text-indigo-600 font-semibold">When will it ever stop?</a>
+        </li>
+        <li>
+          <a href="#" className="text-indigo-600 font-semibold">First post</a>
+        </li>
+      </ul>
+
+      Check out some <a href="#" className="text-indigo-600 font-semibold">older posts &rarr;</a>
     </div>
   )
 }
