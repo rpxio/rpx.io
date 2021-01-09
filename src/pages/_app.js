972d22b0343e776ad2c45f9626360a24cedef2c2
Randall Pace <randall@rpx.io>
2021-01-09 12:16:23 -0500

    add tailwind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '@/css/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>rpx.io</title>
+      </Head>
+      <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        helloworld
+      </h1>
+    </div>
+  )
+}
