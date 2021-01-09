f1f31a86f567c422fc9947d8a82bfb36c8954cf1
Randall Pace <randall@rpx.io>
2021-01-09 13:08:06 -0500

    add tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ export default function Home() {
       <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
         helloworld
       </h1>
-      <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
-        <MarkdownSample />
-      </div>
     </div>
   )
 }
