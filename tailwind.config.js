995b1bf8c66244fbaea5b25da211df433cd5a210
Randall Pace <randall@rpx.io>
2021-01-09 13:07:03 -0500

    add tailwind typography and mdx support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,29 @@
+const mdx = require('@mdx-js/mdx')
+
 module.exports = {
-  purge: ['./src/**/*/*.{js,mdx}'],
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*/*.{js,mdx}'],
+    options: {
+      extractors: [
+        {
+	  extensions: ['mdx'],
+	  extractor: (content) => {
+	    content = mdx.sync(content)
+
+	    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+	    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+
+            return broadMatches.concat(innerMatches)
+	  },
+	},
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +31,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
